694e1fc0ef33de1dcbe16813bd143c0795725237
superflyman90 <superflyman90@gmail.com>
2022-06-23 20:28:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  // Première valeur = valeur du state
+  // Seconde valeur = fonction qui peut mettre à jour le state
+  // Le useState determine la valeur initiale
+
+  const [clicked, plusOne] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>Nombre de fois que tu as cliqué: {clicked}</p>
+      <button onClick={() => {
+        plusOne(clicked + 1)
+      }}> Clique-moi</button>
     </div>
   );
-}
+};
 
 export default App;
